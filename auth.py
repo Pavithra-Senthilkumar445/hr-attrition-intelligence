2e5c15250a88342935d050ef6ac5a69a13873e04
Pavithra-Senthilkumar445 <pavithrasenthilkumar189@gmail.com>
2026-06-14 02:12:20 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -7,9 +7,24 @@ def verify_password(password: str, hashed: str) -> bool:
     return hash_password(password) == hashed
 
 USERS = {
-    "admin@hrapp.com"   : {"password": hash_password("Admin@123"),   "role": "Admin",   "dept": "All",      "name": "Admin User"},
-    "manager@hrapp.com" : {"password": hash_password("Manager@123"), "role": "Manager", "dept": "Sales",    "name": "Sales Manager"},
-    "analyst@hrapp.com" : {"password": hash_password("Analyst@123"), "role": "Analyst", "dept": "All",      "name": "HR Analyst"},
+    "admin@hrapp.com": {
+        "password"  : hash_password("Admin@123"),
+        "role"      : "Admin",
+        "dept"      : "All",
+        "name"      : "Admin User"
+    },
+    "manager@hrapp.com": {
+        "password"  : hash_password("Manager@123"),
+        "role"      : "Manager",
+        "dept"      : "Sales",
+        "name"      : "Sales Manager"
+    },
+    "analyst@hrapp.com": {
+        "password"  : hash_password("Analyst@123"),
+        "role"      : "Analyst",
+        "dept"      : "All",
+        "name"      : "HR Analyst"
+    },
 }
 
 def login(email: str, password: str):
