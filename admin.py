7bee38b85500340b64e3d747e4e1d5ebfe9d7610
Pavithra-Senthilkumar445 <pavithrasenthilkumar189@gmail.com>
2026-06-14 03:45:27 +0530

    Update and rename metrics.py to admin.py

---
+++ admin.py
@@ -0,0 +1,13 @@
+from dash import html, dcc
+import plotly.express as px
+from utils.data_loader import load_data
+
+df = load_data()
+
+def layout():
+    fig = px.histogram(df, x="Age", title="Employee Age Distribution")
+
+    return html.Div([
+        html.H2("Admin Dashboard"),
+        dcc.Graph(figure=fig)
+    ])
